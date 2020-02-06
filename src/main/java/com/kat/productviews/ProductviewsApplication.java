791bbfd5feaf13d79239585b67d40ad25fcef68f
Kat <kat.ochmanska@gmail.com>
2020-02-06 18:52:00 +0100

    First version of application + add missing file

---
+++ src/main/java/com/kat/productviews/ProductviewsApplication.java
@@ -0,0 +1,13 @@
+package com.kat.productviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductviewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kat/productviews/ProductviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat.productviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
